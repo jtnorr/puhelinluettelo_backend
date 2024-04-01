10b419ad28445067b5c96dbd3f7aa304e75b43cb
Jani Norrby <jani.t.norrby@utu.fi>
2024-04-01 15:28:12 +0300

    dist and other changes to accommodate

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(express.static('dist'))
 
 
 let persons = [
@@ -108,7 +109,7 @@ app.delete('/api/persons/:id', (request, response) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
     console.log(morgan('tiny'))
